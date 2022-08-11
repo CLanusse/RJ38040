45589e49090c1225093c15da70219e391612b920
Conrado Lanusse <conrado.lanusse@gmail.com>
2022-08-10 22:10:47 -0300

    Clase 1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+const App = () => {
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className='profe'>
+          Conrado Lanusse the best in the world.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://coderhouse.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Go Coder
         </a>
       </header>
     </div>
